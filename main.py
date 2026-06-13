b9bcc4851584572bc9146f207d595d6cc6c4f84d
Muhammad Zain <zainshykh.1x@gmail.com>
2026-06-13 17:36:49 +0500

    Added schema for data input and response

--- main.py
+++ main.py
@@ -2,35 +2,56 @@ from fastapi import FastAPI, HTTPException, status
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
+from schemas import PostCreate, PostResponse
+from datetime import datetime
 app = FastAPI()
 
 
 posts = [
     {
-        id: 1,
+        "id": 1,
         "title": "title of post 1",
         "content": "content of post 1",
+        "author": "John",
+        "date_posted":str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     },
     {
-        id: 2,
+        "id": 2,
         "title": "title of post 2",
         "content": "content of post 2",
+        "author": "John",
+        "date_posted":str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     }
 ]
 @app.get("/")
 def root():
     return "this is root "
 
-@app.get("/posts")
+@app.get("/posts", response_model=list[PostResponse])
 def get_posts():
-    return {"data": posts}
+    return posts
 
-@app.get("/posts/{id}")
+@app.post("/posts", response_model=PostResponse, status_code=status.HTTP_201_CREATED)
+def create_post(post:PostCreate):
+    print("hello")
+    new_id = max(p["id"] for p in posts)+1 if posts else 1
+    newpost = {
+        "id": new_id,
+        "title":post.title,
+        "content":post.content,
+        "author":post.author,
+        "date_posted": str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    }
+    posts.append(newpost)
+    return newpost
+
+
+@app.get("/posts/{id}", response_model=PostResponse)
 def get_post(id: int):
     for post in posts:
         if post["id"] == id:
-            return {"post_detail": post}
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
+            return post
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
     
 
 @app.exception_handler(StarletteHTTPException)
@@ -46,4 +67,3 @@ def validation_exception_handler(request, exc):
         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
         content={"detail": exc.errors()},
     )
-    
