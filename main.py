0fae2e1de87edeb7d9bc3537b5d36b9a16f5a917
Muhammad Zain <zainshykh.1x@gmail.com>
2026-06-14 01:48:23 +0500

    Database connection and setup

--- main.py
+++ main.py
@@ -1,56 +1,108 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, Depends
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
-from schemas import PostCreate, PostResponse
+from schemas import PostCreate, PostResponse, UserResponse, UserCreate
+from fastapi.staticfiles import StaticFiles
 from datetime import datetime
+from typing import Annotated
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+import models
+from database import get_db, engine, Base
+
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/media", StaticFiles(directory="media"), name="media")
 
-posts = [
-    {
-        "id": 1,
-        "title": "title of post 1",
-        "content": "content of post 1",
-        "author": "John",
-        "date_posted":str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-    },
-    {
-        "id": 2,
-        "title": "title of post 2",
-        "content": "content of post 2",
-        "author": "John",
-        "date_posted":str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-    }
-]
+
 @app.get("/")
 def root():
     return "this is root "
 
 @app.get("/posts", response_model=list[PostResponse])
-def get_posts():
+def get_posts(db:Annotated[Session,Depends(get_db)]):
+    result = db.execute(select(models.Post))
+    posts = result.scalars().all()
     return posts
 
 @app.post("/posts", response_model=PostResponse, status_code=status.HTTP_201_CREATED)
-def create_post(post:PostCreate):
-    print("hello")
-    new_id = max(p["id"] for p in posts)+1 if posts else 1
-    newpost = {
-        "id": new_id,
-        "title":post.title,
-        "content":post.content,
-        "author":post.author,
-        "date_posted": str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-    }
-    posts.append(newpost)
-    return newpost
+def create_post(post:PostCreate, db:Annotated[Session, Depends(get_db)]):
+    result = db.execute(select(models.User).where(models.User.id == post.user_id))
+    user = result.scalars().first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="user with the provided user_id does not exist")
+    new_post = models.Post(
+        title = post.title,
+        content = post.content,
+        user_id = post.user_id
+    )
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return new_post
 
+
+@app.post("/users", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
+def create_user(user:UserCreate, db:Annotated[Session, Depends(get_db)]):
+    result = db.execute(select (models.User).where (models.User.username == user.username),)
+
+    existing_user = result.scalars().first()
+
+    if existing_user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="username already exists")
+    
+    result = db.execute(select (models.User).where (models.User.email == user.email),)
+
+    existing_email = result.scalars().first()
+
+    if existing_email:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="email already exists")
+    
+    new_user = models.User(
+        username = user.username,
+        email = user.email
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
+
+@app.get("/users/{user_id}", response_model=UserResponse)
+def get_user(userid:int, db:Annotated[Session, Depends(get_db)]):
+    result = db.execute(select(models.User).where(models.User.id == userid),)
+
+    user = result.scalars().first()
+
+    if user:
+        return user
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="user was not found")
+
+
+@app.get("/users/{userid}/posts", response_model=list[PostResponse])
+def get_user_posts(userid:int, db:Annotated[Session, Depends(get_db)]):
+    result = db.execute(select(models.User).where(models.User.id == userid))
+    user = result.scalars().first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="user not found")
+    
+    result = db.execute(select (models.Post). where(models.Post.user_id == userid))
+    posts = result.scalars().all()
+    return posts
 
 @app.get("/posts/{id}", response_model=PostResponse)
-def get_post(id: int):
-    for post in posts:
-        if post["id"] == id:
-            return post
+def get_post(id: int, db:Annotated[Session, Depends(get_db)]):
+    result = db.execute(select(models.Post).where(models.Post.id == id))
+    post = result.scalars().first()
+    if post:
+        return post
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
     
 
--- schemas.py
+++ schemas.py
@@ -1,14 +1,33 @@
-from pydantic import BaseModel, Field, ConfigDict
+from pydantic import BaseModel, Field, ConfigDict, EmailStr
+from datetime import datetime
+
+
+class UserBase(BaseModel):
+    username:str = Field(min_length=1, max_length=50)
+    email:EmailStr = Field(max_length=120)
+
+class UserCreate(UserBase):
+    pass
+
+class UserResponse(UserBase):
+    model_config = ConfigDict(from_attributes=True)
+    id:int
+    image_path:str
+    image_file:str|None
+
+
+
 
 class PostBase(BaseModel):
     title:str = Field(min_length=1, max_length=100)
-    author:str = Field(min_length=1, max_length=50)
     content:str = Field(min_length=10)
 
 class PostCreate(PostBase):
-    pass
+    user_id:int            #temporary until we implement authentication
 
 class PostResponse(PostBase):
     model_config = ConfigDict(from_attributes=True)
     id:int
-    date_posted:str
+    userid:int
+    date_posted:datetime
+    author:UserResponse
